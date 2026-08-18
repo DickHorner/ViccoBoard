e03e0124d19f18644279b6a150676a8f3514de79
J Luetkens <megahoschi@googlemail.com>
2026-08-19 00:25:01 +0200

    Combine Methodenfundgrube sport seed data

---
+++ apps/teacher-ui/src/data/methodenfundgrube-seed-data.ts
@@ -0,0 +1,33 @@
+import { METHODENFUNDGRUBE_CHAPTER_1 } from './methodenfundgrube-chapter-1';
+import { METHODENFUNDGRUBE_CHAPTER_2 } from './methodenfundgrube-chapter-2';
+import { METHODENFUNDGRUBE_CHAPTER_3 } from './methodenfundgrube-chapter-3';
+import { METHODENFUNDGRUBE_CHAPTER_4A } from './methodenfundgrube-chapter-4a';
+import { METHODENFUNDGRUBE_CHAPTER_4B } from './methodenfundgrube-chapter-4b';
+import { METHODENFUNDGRUBE_CHAPTER_5A } from './methodenfundgrube-chapter-5a';
+import { METHODENFUNDGRUBE_CHAPTER_5B } from './methodenfundgrube-chapter-5b';
+import { METHODENFUNDGRUBE_CHAPTER_5C } from './methodenfundgrube-chapter-5c';
+import { METHODENFUNDGRUBE_CHAPTER_5D } from './methodenfundgrube-chapter-5d';
+import { METHODENFUNDGRUBE_CHAPTER_6A } from './methodenfundgrube-chapter-6a';
+import { METHODENFUNDGRUBE_CHAPTER_6B } from './methodenfundgrube-chapter-6b';
+import { METHODENFUNDGRUBE_CHAPTER_6C } from './methodenfundgrube-chapter-6c';
+import { METHODENFUNDGRUBE_CHAPTER_7A } from './methodenfundgrube-chapter-7a';
+import { METHODENFUNDGRUBE_CHAPTER_7B } from './methodenfundgrube-chapter-7b';
+import { METHODENFUNDGRUBE_CHAPTER_7C } from './methodenfundgrube-chapter-7c';
+
+export const METHODENFUNDGRUBE_SEED_DATA = [
+  ...METHODENFUNDGRUBE_CHAPTER_1,
+  ...METHODENFUNDGRUBE_CHAPTER_2,
+  ...METHODENFUNDGRUBE_CHAPTER_3,
+  ...METHODENFUNDGRUBE_CHAPTER_4A,
+  ...METHODENFUNDGRUBE_CHAPTER_4B,
+  ...METHODENFUNDGRUBE_CHAPTER_5A,
+  ...METHODENFUNDGRUBE_CHAPTER_5B,
+  ...METHODENFUNDGRUBE_CHAPTER_5C,
+  ...METHODENFUNDGRUBE_CHAPTER_5D,
+  ...METHODENFUNDGRUBE_CHAPTER_6A,
+  ...METHODENFUNDGRUBE_CHAPTER_6B,
+  ...METHODENFUNDGRUBE_CHAPTER_6C,
+  ...METHODENFUNDGRUBE_CHAPTER_7A,
+  ...METHODENFUNDGRUBE_CHAPTER_7B,
+  ...METHODENFUNDGRUBE_CHAPTER_7C,
+];
